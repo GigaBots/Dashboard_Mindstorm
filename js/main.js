9cca82827073e5c3f44daf50b897cc4f04b3cdd8
Cole Bittel <cole.bittel@gmail.com>
2014-04-29 22:04:30 -0500

    Moving progress bar
    progress bar to update and move across screen linearly

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+	$("#progressbar").progressbar({value: 10});
+    var progress = setInterval(function() {
+   var currentVal = $("#progressbar").progressbar("option", "value");
+   var nextVal = currentVal + 1;
+   if (nextVal > 100) {
+       clearInterval(progress);
+   } else {
+       $("#progressbar").progressbar({value: nextVal});
+   }
+}, 100);
+});
+
+

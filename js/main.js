9d71aa084f3b250112375cba59ad64bd6fe14493
John DiBaggio <jsantoku@gmail.com>
2014-06-02 16:31:12 -0500

    fixed stop overlay size
    fixed stop overlay size

--- js/main.js
+++ js/main.js
@@ -1312,7 +1312,7 @@ require(['BrowserBigBangClient'], function (bigbang) {
                 status.statusDisplay = game.add.text(positionStatus.x+12, positionStatus.y+30, labelStatusDisplay, labelStyle);
                 resume.resumeOverlay = game.add.graphics(0,0);
                 resume.resumeOverlay.beginFill(0x00000,0.45);
-                resume.resumeOverlay.drawRect(0,51,960,659);
+                resume.resumeOverlay.drawRect(0,51,960,599);
                 resume.resumeMessageDisplay = game.add.sprite(gameBoundX/2-251,280,'resume');
                 this.game.input.keyboard.disabled = true;
             } else {
